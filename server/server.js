155e419a5501734d4404a1a1f399847dff9b2c74
luanadriani <luanadriani2011@gmail.com>
2021-11-09 18:03:58 -0300

    Conexão com mongoDB

--- server/server.js
+++ server/server.js
@@ -2,14 +2,24 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
+const mongoose = require('mongoose')
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+mongoose.connect('mongodb://localhost:27017/contagem-carbo', {
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}, function (err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log('MongoDB Conectado com sucesso!')
+    }
+})
+
 app.use(cors());
-
 app.use(cookieParser());
-
 app.use(express.json());
 
 app.get('/', function(req, res){
